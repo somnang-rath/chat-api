d480eae22826385c9bc4a3a22dd7bcdd04b6759a
Rath Somnang <samnang.rath@onego.com.kh>
2025-11-26 10:34:50 +0700

    socket

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -4,13 +4,21 @@ export const socketHandler = (io: Server) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id)
 
+    // =========================
+    // Join chat room
+    // =========================
     socket.on("join_chat", (chatId) => {
       socket.join(chatId)
       console.log("Joined chat:", chatId)
     })
 
+    // =========================
+    // Send message in chat
+    // =========================
     socket.on("send_message", (data) => {
+      // Emit to the chat room
       io.to(data.chatId).emit("new_message", data)
+      console.log("Sent message to room:", data.chatId)
     })
 
     socket.on("disconnect", () => {
